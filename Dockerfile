de10782f79ac34451d6bc4945e41cb0d6b2af29c
Murugesan-kanyakumari <murugesanir1997.gmail.com>
2025-05-22 12:37:03 +0530

    updated pm2

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ COPY . .
 
 EXPOSE 5000
 
-CMD ["pm2-runtime", "ecosystem.config.js"]
+CMD ["pm2-runtime", "ecosystem.config.cjs"]
--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => res.send('API running'));
+app.get('/', (req, res) => res.send({ data: 'API running' }));
 
 export default app;
